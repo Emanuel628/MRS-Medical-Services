5b2ba1c492154eb12a95aac0b04457db3e4aacd9
Emanuel628 <144055605+Emanuel628@users.noreply.github.com>
2026-07-31 15:33:00 -0400

    Load landing page adjustment styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import './styles/reset.css';
 import './styles/variables.css';
 import './styles/global.css';
 import './styles/responsive.css';
+import './styles/adjustments.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
