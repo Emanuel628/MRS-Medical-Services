b16c44d3c2d350287cb1dd81a71daf48b16aa51f
Emanuel628 <144055605+Emanuel628@users.noreply.github.com>
2026-08-17 09:30:35 -0400

    Enable temporary frontend checkout block

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './temporaryStripeCheckoutBlock';
 import './styles/reset.css';
 import './styles/variables.css';
 import './styles/global.css';
