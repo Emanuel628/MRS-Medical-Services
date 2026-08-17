4a363eafd5ff35e5fe96497d2dea19a682030069
Emanuel628 <144055605+Emanuel628@users.noreply.github.com>
2026-08-17 09:39:08 -0400

    Restore client bootstrap after failed build

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './temporaryStripeCheckoutBlock';
 import './styles/reset.css';
 import './styles/variables.css';
 import './styles/global.css';
